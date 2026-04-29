9441eb695f27c7eef9e867544af3dbc226197660
Fahmi Ramadhan <fahmimustamar58@gmail.com>
2026-04-29 08:41:59 +0700

    Stecu-RoleAuth

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,6 +16,7 @@ class User extends Authenticatable
         'phone',
         'address',
         'password',
+        'role',
     ];
 
     protected $hidden = [
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
             $table->string('phone')->unique()->nullable();
             $table->string('address')->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'customer'])->default('customer'); // ← role field
             $table->rememberToken();
             $table->timestamps();
         });
